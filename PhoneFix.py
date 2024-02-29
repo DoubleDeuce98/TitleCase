8ae875866b6b22404b9ee3931b70eed5e17d3eb2
DoubleDeuce98 <127911610+DoubleDeuce98@users.noreply.github.com>
2024-02-29 10:55:30 -0800

    Update PhoneFix.py
    Mostly fixed all formatting errors

--- PhoneFix.py
+++ PhoneFix.py
@@ -8,11 +8,23 @@ def main():
 
     #Remove obnoxious North American area code
     #Maybe should look into making the area code removable regardless of location
-    entry = phone.removeprefix("+1")
+    entry = phone.removeprefix("+1").strip()
 
-    #Removing whitespace and adding dashes in correct spots
-    entry = entry.replace("(", "").replace(") ", "-").strip()
+    #Removing parenthasis and adding dashes in correct spots
+    entry = entry.replace("(", "-").replace(")", "-").strip("-")
 
+    #This is for if they botched the parenthasis when typing
+    #This is becoming a mess
+    entry = entry.replace("- ", "-")
+    
+    #If decimal points were used, replace them with dashes
+    entry = entry.replace(".", "-")
+    
+    
+    #This needs to be at the end to check for double dashes
+    entry = entry.replace("--", "-")
+    
+    #Print the completed result
     print()
     print(entry)
     print()
